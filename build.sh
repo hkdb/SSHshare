aa8ff3bd93f1e6c87e808133c4670ed9ee0008f7
hkdb <hkdb@3df.io>
2020-05-28 14:59:47 +0800

    Checked build on High Sierra

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     go build -o linux/SSHshare
 elif [[ "$OSTYPE" == "darwin19" ]]; then
     go build -o macos/SSHshare
+elif [[ "$OSTYPE" == "darwin17" ]]; then
+    go build -o macos/SSHshare
 elif [[ "$OSTYPE" == "win32" ]]; then
     go build -ldflags="-H windowsgui" -o win/SSHshare
 else
